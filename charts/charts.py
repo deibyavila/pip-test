a7e78e40595c10378a0ba8566b9ee45c283aa2db
Deiby <deibyavila@gmail.com>
2023-07-02 15:54:43 -0500

    firts matplotlib

---
+++ charts/charts.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+
+
+def generate_pie_chart():
+    labels = ['a','b','c']
+    values = [200,300,400]
+
+    fig, ax = plt.subplots()
+    ax.pie(values, labels= labels)
+    plt.savefig('pie.png')
+    plt.close()
